c4b52fd37e6a061b2106bc0a2ce7c1f6ce1a0ec8
Yair <yair.rovero@gmail.com>
2019-03-20 23:07:28 -0600

    Se ejecutó el comando chido y se creo la migracion para price

---
+++ database/migrations/2019_03_21_043948_create_prices_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePricesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prices', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('year')->nullable();
+            $table->decimal('price',10,2);
+            $table->integer('construction_id')->unsigned();
+            $table->integer('provider_id')->unsigned();
+            $table->integer('unity_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('construction_id')->references('id')->on('constructions')
+              ->onDelete('cascade')
+              ->onUpdate('cascade');
+            $table->foreign('provider_id')->references('id')->on('providers')
+              ->onDelete('cascade')
+              ->onUpdate('cascade');
+              $table->foreign('unity_id')->references('id')->on('unities')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prices');
+    }
+}
